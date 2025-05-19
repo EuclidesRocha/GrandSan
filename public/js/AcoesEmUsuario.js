72644869a6a640ef379826ae64e5d4b56f6f8926
Calil30 <euclides.rocha@sptech.school>
2025-05-19 16:11:43 -0300

    Adicionando fonte correta

--- public/js/AcoesEmUsuario.js
+++ public/js/AcoesEmUsuario.js
@@ -2,18 +2,18 @@ function mudarTelaUser(){
     var username = sessionStorage.NOME_USUARIO;
   var  userCidadePrefe =  sessionStorage.CIDADE_USUARIO;
    if (userCidadePrefe == 1){
-    	userimagem = `url('../arquivos/imagens/imagemLossantos.png')`;
-        div_home.style.backgroundImage = userimagem;
+    	var userimagemFundo = `url('../arquivos/imagens/imagemLossantos.png')`;
+        div_home.style.backgroundImage = userimagemFundo;
         p_cidade.innerHTML += ` LOS SANTOS`
         
 
    }else if(userCidadePrefe == 2){
-    	userimagem = `url('../arquivos/imagens/imagemSanfierro.png')`;
-        div_home.style.backgroundImage = userimagem;
+    	var userimagemFundo = `url('../arquivos/imagens/imagemSanfierro.png')`;
+        div_home.style.backgroundImage = userimagemFundo;
         p_cidade.innerHTML += ` SAN FIERRO`
    }else if(userCidadePrefe == 3){
-    	userimagem = `url('../arquivos/imagens/imagemLasVenturas.png')`;
-        div_home.style.backgroundImage = userimagem;
+    	var userimagemFundo = `url('../arquivos/imagens/imagemLasVenturas.png')`;
+        div_home.style.backgroundImage = userimagemFundo;
         p_cidade.innerHTML += `LAS VENTURAS`
    }
    p_ola.innerHTML += ` ${username}`; 
@@ -58,18 +58,18 @@ function mudarTelaUser(){
     var color = "color"
     if(userGangue == 1){
         color = "rgba(10, 60, 10, 0.8)"
-        
+        imagemUser.style.backgroundImage = `url('../arquivos/imagens/imagemSanfierro.png')`
         div_corDasPaginas.style.backgroundColor = color;
         
 
     }else if(userGangue == 2){
         color = "rgba(61, 7, 48, 0.8)"
-        
+        imagemUser.style.backgroundImage = `url('../arquivos/imagens/imagemSanfierro.png')`
         div_corDasPaginas.style.backgroundColor = color;
       
     }else if(userGangue == 3){
         color = "rgba(179, 184, 27, 0.8)"
-      
+        imagemUser.style.backgroundImage = `url('../arquivos/imagens/imagemSanfierro.png')`
         div_corDasPaginas.style.backgroundColor = color;
         
     
@@ -77,7 +77,7 @@ function mudarTelaUser(){
 
     }else if(userGangue == 4){
         color = "rgba(48, 186, 217, 0.8)"
-       
+       imagemUser.style.backgroundImage = `url('../arquivos/imagens/imagemSanfierro.png')`
         div_corDasPaginas.style.backgroundColor = color;
       
 
