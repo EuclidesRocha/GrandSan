98fec9696ebcd9011b2346e02a132c815b97f602
EuclidesRocha <euclides.rocha@sptech.school>
2025-05-20 00:19:28 -0300

    correcão de envio

--- public/js/MissoesAcoes.js
+++ public/js/MissoesAcoes.js
@@ -1,281 +1,281 @@
-color = "rgba(10, 60, 10, 0.8)"
+color = "rgba(10, 60, 10, 1)"
 
 function feita1(){
-missao1.style.backgroudColor = color
+id_missao1.style.backgroudColor = color
 }function feita2(){
-    missao2.style.backgroudColor = color
+    id_missao2.style.backgroudColor = color
 }function feita3(){
-    missao3.style.backgroudColor = color
+    id_missao3.style.backgroudColor = color
 }function feita4(){
-    missao4.style.backgroudColor = color
+    id_missao4.style.backgroudColor = color
 }function feita5(){
-    missao5.style.backgroudColor = color
+    id_missao5.style.backgroudColor = color
 }function feita6(){
-    missao6.style.backgroudColor = color
+    id_missao6.style.backgroudColor = color
 }function feita7(){
-    missao7.style.backgroudColor = color
+    id_missao7.style.backgroudColor = color
 }function feita8(){
-    missao8.style.backgroudColor = color
+    id_missao8.style.backgroudColor = color
 }function feita9(){
-    missao9.style.backgroudColor = color
+    id_missao9.style.backgroudColor = color
 }function feita10(){
-    missao10.style.backgroudColor = color
+    id_missao10.style.backgroudColor = color
 }function feita11(){
-    missao11.style.backgroudColor = color
+    id_missao11.style.backgroudColor = color
 }function feita12(){
-    missao12.style.backgroudColor = color
+    id_missao12.style.backgroudColor = color
 }function feita13(){
-    missao13.style.backgroudColor = color
+    id_missao13.style.backgroudColor = color
 }function feita14(){
-    missao14.style.backgroudColor = color
+    id_missao14.style.backgroudColor = color
 }function feita15(){
-    missao15.style.backgroudColor = color
+    id_missao15.style.backgroudColor = color
 }function feita16(){
-    missao16.style.backgroudColor = color
+    id_missao16.style.backgroudColor = color
 }function feita17(){
-    missao17.style.backgroudColor = color
+    id_missao17.style.backgroudColor = color
 }function feita18(){
-    missao18.style.backgroudColor = color
+    id_missao18.style.backgroudColor = color
 }function feita19(){
-    missao19.style.backgroudColor = color
+    id_missao19.style.backgroudColor = color
 }function feita20(){
-    missao20.style.backgroudColor = color
+    id_missao20.style.backgroudColor = color
 }function feita21(){
-    missao21.style.backgroudColor = color
+    id_missao21.style.backgroudColor = color
 }function feita22(){
-    missao22.style.backgroudColor = color
+    id_missao22.style.backgroudColor = color
 }function feita23(){
-    missao23.style.backgroudColor = color
+    id_missao23.style.backgroudColor = color
 }function feita24(){
-    missao24.style.backgroudColor = color
+    id_missao24.style.backgroudColor = color
 }function feita25(){
-    missao25.style.backgroudColor = color
+    id_missao25.style.backgroudColor = color
 }function feita26(){
-    missao26.style.backgroudColor = color
+    id_missao26.style.backgroudColor = color
 }function feita27(){
-    missao27.style.backgroudColor = color
+    id_missao27.style.backgroudColor = color
 }function feita28(){
-    missao28.style.backgroudColor = color
+    id_missao28.style.backgroudColor = color
 }function feita29(){
-    missao29.style.backgroudColor = color
+    id_missao29.style.backgroudColor = color
 }function feita30(){
-    missao30.style.backgroudColor = color
+    id_missao30.style.backgroudColor = color
 }function feita31(){
-    missao31.style.backgroudColor = color
+    id_missao31.style.backgroudColor = color
 }function feita32(){
-    missao32.style.backgroudColor = color
+    id_missao32.style.backgroudColor = color
 }function feita33(){
-    missao33.style.backgroudColor = color
+    id_missao33.style.backgroudColor = color
 }function feita34(){
-    missao34.style.backgroudColor = color
+    id_missao34.style.backgroudColor = color
 }function feita35(){
-    missao35.style.backgroudColor = color
+    id_missao35.style.backgroudColor = color
 }function feita36(){
-    missao36.style.backgroudColor = color
+    id_missao36.style.backgroudColor = color
 }function feita37(){
-    missao37.style.backgroudColor = color
+    id_missao37.style.backgroudColor = color
 }function feita38(){
-    missao38.style.backgroudColor = color
+    id_missao38.style.backgroudColor = color
 }function feita39(){
-    missao39.style.backgroudColor = color
+    id_missao39.style.backgroudColor = color
 }function feita40(){
-    missao40.style.backgroudColor = color
+    id_missao40.style.backgroudColor = color
 }function feita41(){
-    missao41.style.backgroudColor = color
+    id_missao41.style.backgroudColor = color
 }function feita42(){
-    missao42.style.backgroudColor = color
+    id_missao42.style.backgroudColor = color
 }function feita43(){
-    missao43.style.backgroudColor = color
+    id_missao43.style.backgroudColor = color
 }function feita44(){
-    missao44.style.backgroudColor = color
+    id_missao44.style.backgroudColor = color
 }function feita45(){
-    missao45.style.backgroudColor = color
+    id_missao45.style.backgroudColor = color
 }function feita46(){
-    missao46.style.backgroudColor = color
+    id_missao46.style.backgroudColor = color
 }function feita47(){
-    missao47.style.backgroudColor = color
+    id_missao47.style.backgroudColor = color
 }function feita48(){
-    missao48.style.backgroudColor = color
+    id_missao48.style.backgroudColor = color
 }function feita49(){
-    missao49.style.backgroudColor = color
+    id_missao49.style.backgroudColor = color
 }function feita50(){
-    missao50.style.backgroudColor = color
+    id_missao50.style.backgroudColor = color
 }function feita51(){
-    missao51.style.backgroudColor = color
+    id_missao51.style.backgroudColor = color
 }function feita52(){
-    missao52.style.backgroudColor = color
+    id_missao52.style.backgroudColor = color
 }function feita53(){
-    missao53.style.backgroudColor = color
+    id_missao53.style.backgroudColor = color
 }function feita54(){
-    missao54.style.backgroudColor = color
+    id_missao54.style.backgroudColor = color
 }function feita55(){
-    missao55.style.backgroudColor = color
+    id_missao55.style.backgroudColor = color
 }function feita56(){
-    missao56.style.backgroudColor = color
+    id_missao56.style.backgroudColor = color
 }function feita57(){
-    missao57.style.backgroudColor = color
+    id_missao57.style.backgroudColor = color
 }function feita58(){
-    missao58.style.backgroudColor = color
+    id_missao58.style.backgroudColor = color
 }function feita59(){
-    missao59.style.backgroudColor = color
+    id_missao59.style.backgroudColor = color
 }function feita60(){
-missao60.style.backgroudColor = color
+id_missao60.style.backgroudColor = color
 }function feita61() {
-missao61.style.backgroudColor = color
+id_missao61.style.backgroudColor = color
 }
 
 function feita62() {
-missao62.style.backgroudColor = color
+id_missao62.style.backgroudColor = color
 }
 
 function feita63() {
-missao63.style.backgroudColor = color
+id_missao63.style.backgroudColor = color
 }
 
 function feita64() {
-missao64.style.backgroudColor = color
+id_missao64.style.backgroudColor = color
 }
 
 function feita65() {
-missao65.style.backgroudColor = color
+id_missao65.style.backgroudColor = color
 }
 
 function feita66() {
-missao66.style.backgroudColor = color
+id_missao66.style.backgroudColor = color
 }
 
 function feita67() {
-missao67.style.backgroudColor = color
+id_missao67.style.backgroudColor = color
 }
 
 function feita68() {
-missao68.style.backgroudColor = color
+id_missao68.style.backgroudColor = color
 }
 
 function feita69() {
-missao69.style.backgroudColor = color
+id_missao69.style.backgroudColor = color
 }
 
 function feita70() {
-missao70.style.backgroudColor = color
+id_missao70.style.backgroudColor = color
 }
 
 function feita71() {
-missao71.style.backgroudColor = color
+id_missao71.style.backgroudColor = color
 }
 
 function feita72() {
-missao72.style.backgroudColor = color
+id_missao72.style.backgroudColor = color
 }
 
 function feita73() {
-missao73.style.backgroudColor = color
+id_missao73.style.backgroudColor = color
 }
 
 function feita74() {
-missao74.style.backgroudColor = color
+id_missao74.style.backgroudColor = color
 }
 
 function feita75() {
-missao75.style.backgroudColor = color
+id_missao75.style.backgroudColor = color
 }
 
 function feita76() {
-missao76.style.backgroudColor = color
+id_missao76.style.backgroudColor = color
 }
 
 function feita77() {
-missao77.style.backgroudColor = color
+id_missao77.style.backgroudColor = color
 }
 
 function feita78() {
-missao78.style.backgroudColor = color
+id_missao78.style.backgroudColor = color
 }
 
 function feita79() {
-missao79.style.backgroudColor = color
+id_missao79.style.backgroudColor = color
 }
 
 function feita80() {
-missao80.style.backgroudColor = color
+id_missao80.style.backgroudColor = color
 }
 
 function feita81() {
-missao81.style.backgroudColor = color
+id_missao81.style.backgroudColor = color
 }
 
 function feita82() {
-missao82.style.backgroudColor = color
+id_missao82.style.backgroudColor = color
 }
 
 function feita83() {
-missao83.style.backgroudColor = color
+id_missao83.style.backgroudColor = color
 }
 
 function feita84() {
-missao84.style.backgroudColor = color
+id_missao84.style.backgroudColor = color
 }
 
 function feita85() {
-missao85.style.backgroudColor = color
+id_missao85.style.backgroudColor = color
 }
 
 function feita86() {
-missao86.style.backgroudColor = color
+id_missao86.style.backgroudColor = color
 }
 
 function feita87() {
-missao87.style.backgroudColor = color
+id_missao87.style.backgroudColor = color
 }
 
 function feita88() {
-missao88.style.backgroudColor = color
+id_missao88.style.backgroudColor = color
 }
 
 function feita89() {
-missao89.style.backgroudColor = color
+id_missao89.style.backgroudColor = color
 }
 
 function feita90() {
-missao90.style.backgroudColor = color
+id_missao90.style.backgroudColor = color
 }
 
 function feita91() {
-missao91.style.backgroudColor = color
+id_missao91.style.backgroudColor = color
 }
 
 function feita92() {
-missao92.style.backgroudColor = color
+id_missao92.style.backgroudColor = color
 }
 
 function feita93() {
-missao93.style.backgroudColor = color
+id_missao93.style.backgroudColor = color
 }
 
 function feita94() {
-missao94.style.backgroudColor = color
+id_missao94.style.backgroudColor = color
 }
 
 function feita95() {
-missao95.style.backgroudColor = color
+id_missao95.style.backgroudColor = color
 }
 
 function feita96() {
-missao96.style.backgroudColor = color
+id_missao96.style.backgroudColor = color
 }
 
 function feita97() {
-missao97.style.backgroudColor = color
+id_missao97.style.backgroudColor = color
 }
 
 function feita98() {
-missao98.style.backgroudColor = color
+id_missao98.style.backgroudColor = color
 }
 
 function feita99() {
-missao99.style.backgroudColor = color
+id_missao99.style.backgroudColor = color
 }
 
 function feita100() {
-missao100.style.backgroudColor = color
+id_missao100.style.backgroudColor = color
 }
