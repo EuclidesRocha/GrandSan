ce44c25038fade872ad201151fe277113154762e
EuclidesRocha <euclides.rocha@sptech.school>
2025-05-25 15:28:25 -0300

    Adicionando mais imagens, correção de cores e correção de validar sessão

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,12 +2,16 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
+   
 
     if (email != null && nome != null) {
+
+        return true
         
     } else {
-        window.location = "../TelaLogin.html";
+         window.location = "../TelaLogin.html";
+        return false
+       
     }
 }
 
