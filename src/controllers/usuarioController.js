86ad5d243e8500e005cb0b60607ff68ee9ecbf8b
EuclidesRocha <euclides.rocha@sptech.school>
2025-05-27 23:05:09 -0300

    tela enciclopedia/ itens feita. E Novas estrutura do banco de dados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -31,8 +31,10 @@ function autenticar(req, res) {
                            
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
+
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+
                     }
                 }
             ).catch(
--- src/models/missoesModel.js
+++ src/models/missoesModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 
 function listar(idUsuario, missao) {
     var instrucao = `
-        SELECT numero, concluida FROM missoes where fkUsuario = ${idUsuario} and numero = ${missao};
+        SELECT fkMissoes, concluida FROM StatusMissao where fkUsuario = ${idUsuario} and fkMissoes = ${missao};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -14,7 +14,7 @@ function cadastrarPrimeiraVez(idUsuario, missao	) {
     
         
     var instrucao = `
-        INSERT INTO missoes (numero, concluida, fkUsuario) VALUES ( ${missao}, 0,'${idUsuario}');
+        INSERT INTO StatusMissao (fkMissoes, fkUsuario, concluida ) VALUES ( ${missao},'${idUsuario}', 0);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -26,7 +26,7 @@ function cadastrarPrimeiraVez(idUsuario, missao	) {
 
 function atualizar(idUsuario, missao, concluida){
     var instrucao = `
-        UPDATE missoes SET concluida = ${concluida} where numero = ${missao} and fkUsuario =${idUsuario};
+        UPDATE StatusMissao SET concluida = ${concluida} where fkMissoes= ${missao} and fkUsuario =${idUsuario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
